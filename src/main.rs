359f5b8076dafeb10227ba94a01abadbbea12e93
Tim Raymond <tim@timraymond.com>
2020-06-20 11:48:15 -0400

    Add implementation of Vec3
    In order to more properly do math on vectors, it makes sense to have a
    vector type to handle operations with other vectors. This introduces
    Vec3, a named tuple, that has overloaded operators for performing math
    on other Vec3s

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,195 @@
+use std::ops;
+
+#[derive(PartialEq,Debug)]
+struct Vec3(f64, f64, f64);
+
+impl Vec3 {
+    fn unit() -> Vec3 {
+        Vec3(1.0, 1.0, 1.0)
+    }
+
+    fn dot(&self, _rhs: &Vec3) -> f64 {
+        self.0 * _rhs.0 +
+        self.1 * _rhs.1 +
+        self.2 * _rhs.2
+    }
+
+    fn len(&self) -> f64 {
+        (self.0*self.0 +
+         self.1*self.1 +
+         self.2*self.2).sqrt()
+    }
+
+    fn cross(&self, _rhs: Vec3) -> Vec3 {
+        Vec3(self.1*_rhs.2 - self.2*_rhs.1,
+             -(self.0*_rhs.2 - self.2*_rhs.0),
+             self.0*_rhs.1 - self.1*_rhs.0)
+    }
+}
+
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, _rhs: Vec3) -> Vec3 {
+        Vec3(self.0 + _rhs.0,
+             self.1 + _rhs.1,
+             self.2 + _rhs.2)
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, _rhs: Vec3) -> Vec3 {
+        Vec3(self.0 - _rhs.0,
+             self.1 - _rhs.1,
+             self.2 - _rhs.2)
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, _rhs: Vec3) -> Vec3 {
+        Vec3(self.0 * _rhs.0,
+             self.1 * _rhs.1,
+             self.2 * _rhs.2)
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, _rhs: f64) -> Vec3 {
+        Vec3(self.0 * _rhs,
+             self.1 * _rhs,
+             self.2 * _rhs)
+    }
+}
+
+impl ops::Div<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, _rhs: Vec3) -> Vec3 {
+        Vec3(self.0 / _rhs.0,
+             self.1 / _rhs.1,
+             self.2 / _rhs.2)
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, _rhs: f64) -> Vec3 {
+        Vec3(self.0 / _rhs,
+             self.1 / _rhs,
+             self.2 / _rhs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Vec3;
+
+    #[test]
+    fn test_add() {
+        let a = Vec3(1.0, 1.0, 1.0);
+        let b = Vec3(1.0, 1.0, 1.0);
+        let c = a + b;
+
+        assert_eq!(2.0, c.0);
+        assert_eq!(2.0, c.1);
+        assert_eq!(2.0, c.2);
+    }
+
+    #[test]
+    fn test_sub() {
+        let a = Vec3(2.0, 2.0, 2.0);
+        let b = Vec3(2.0, 2.0, 2.0);
+        let c = a - b;
+
+        assert_eq!(0.0, c.0);
+        assert_eq!(0.0, c.1);
+        assert_eq!(0.0, c.2);
+    }
+
+    #[test]
+    fn test_mul_vec() {
+        let a = Vec3(2.0, 2.0, 2.0);
+        let b = Vec3(2.0, 2.0, 2.0);
+        let c = a * b;
+
+        assert_eq!(4.0, c.0);
+        assert_eq!(4.0, c.1);
+        assert_eq!(4.0, c.2);
+    }
+
+    #[test]
+    fn test_mul_scalar() {
+        let a = Vec3(2.0, 2.0, 2.0);
+        let c = a * 2.0;
+
+        assert_eq!(4.0, c.0);
+        assert_eq!(4.0, c.1);
+        assert_eq!(4.0, c.2);
+    }
+
+    #[test]
+    fn test_div_vec() {
+        let a = Vec3(2.0, 2.0, 2.0);
+        let b = Vec3(2.0, 2.0, 2.0);
+        let c = a / b;
+
+        assert_eq!(1.0, c.0);
+        assert_eq!(1.0, c.1);
+        assert_eq!(1.0, c.2);
+    }
+
+    #[test]
+    fn test_div_scalar() {
+        let a = Vec3(2.0, 2.0, 2.0);
+        let c = a / 2.0;
+
+        assert_eq!(1.0, c.0);
+        assert_eq!(1.0, c.1);
+        assert_eq!(1.0, c.2);
+    }
+
+    #[test]
+    fn test_dot() {
+        let a = Vec3(2.0, 2.0, 2.0);
+        let b = Vec3(2.0, 2.0, 2.0);
+
+        assert_eq!(12.0, a.dot(&b));
+    }
+
+    #[test]
+    fn test_unit() {
+        let a = Vec3::unit();
+
+        assert_eq!(1.0, a.0);
+        assert_eq!(1.0, a.1);
+        assert_eq!(1.0, a.2);
+    }
+
+    #[test]
+    fn test_len() {
+        let a = Vec3::unit();
+
+        assert_eq!(1732, (a.len() * 1000.0) as i32);
+    }
+
+    #[test]
+    fn test_cross() {
+        let a = Vec3(3.0, 2.0, -2.0);
+        let b = Vec3(1.0, 0.0, -5.0);
+
+        let exp = Vec3(-10.0, 13.0, -2.0);
+
+        assert_eq!(exp, a.cross(b));
+    }
+}
+
 fn main() {
     // define extents
     let nx = 200;
@@ -16,14 +208,18 @@ fn main() {
     for j in (0..ny).rev() {
         for i in 0..nx {
             // compute color
-            let r = i as f32 / nx as f32;
-            let g = j as f32 / ny as f32;
-            let b = 0.2 as f32;
+            let col = Vec3(
+                i as f64 / nx as f64,
+                j as f64 / ny as f64,
+                0.2
+            );
 
             // compute intensity
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let intensity = col * 255.99;
+
+            let ir = intensity.0 as i32;
+            let ig = intensity.1 as i32;
+            let ib = intensity.2 as i32;
 
             println!("{} {} {}", ir, ig, ib);
         }
